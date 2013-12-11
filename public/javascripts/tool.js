bf4e9cb602ac3a24ab2bd0488583be3cd425dea4
Rene <rene@renes-mbp.fritz.box>
2013-12-11 22:17:22 +0100

    dialog jquery-ui loaded from server

--- public/javascripts/tool.js
+++ public/javascripts/tool.js
@@ -6,16 +6,47 @@
 var onCmdClick = function(item) {
 	console.log(item);
 	var cmd = $(item).attr('cmd');
-	console.log("clicked on element with cmd:" + cmd);
+//	console.log("clicked on element with cmd:" + cmd);
 	$.get( "/command", { "cmd": cmd } ).done( function(r) {
 		var content = r["content"];
 		$('#ce_content').html(content);
+		var dialog = r["dialog"];
+		if (dialog) {
+			$("#"+dialog).dialog({
+				modal: true,
+				buttons: {
+					Ok: function() {
+						$(this).dialog( "close" );
+					}
+				}
+			});
+		}
 	} );
 }
 
 
 
+// simple show dialog
+var onShowDialog = function(item) {
+	console.log(item);
+	var dialogId = $(item).attr('dialog');
+	console.log("show dialog:" +  dialogId);
+	var sel = "#" + dialogId;
+	$( "#"+dialogId ).dialog({
+		modal: true,
+		buttons: {
+			Ok: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+	});
+	
+}
+
+
+
 $(document).ready( function() {
+	// $(".dialog").display(false);
 	// if doing so, than $(this) in onCmdClick() is set on 
 	// the correct element
 	// but if the element contains onclick="onCmdClick()" than $(this) is not ok
--- routes/command.js
+++ routes/command.js
@@ -7,18 +7,31 @@ function CommandHandler(db) {
 		var cmd = req.query.cmd;
 //		console.log("handleCmd:" + cmd);
 
+		
 		var content = "Cmd:" + cmd;
 		var menu = "";
+		var dialog = undefined;
 		switch (cmd) {
 			case "login":
 				content ='<a href="#" class="ce_cmd" onclick="onCmdClick(this)" cmd="parts">PartsManagement</a>';
+
 				break;
 
 			case "parts":
 				content = 'Das sind jetzt Artikel';
 				break;
+
+			case "showdialog":
+			content = 
+' <div class="dialog" id="dialog-a" title="new dialog"> 	<p> 	\
+	<span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span> 	\
+		Your files have downloaded successfully into the My Downloads folder. 	</p> 	<p> 	\
+			Currently using <b>36% of your storage space</b>. 	</p> </div> ';
+			dialog = "dialog-a";
+
 		}
-		var data = {content: content, menu:menu};
+		console.log("Dialog:" + dialog);
+		var data = {content: content, menu:menu, dialog:dialog};
 		res.send(data);
 	}
 };
